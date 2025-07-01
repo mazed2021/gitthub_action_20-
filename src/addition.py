b0d750442de23d66cf5e0a910e6aa3cd360e9d96
mazed2021 <mdmazedulislam889@gmail.com>
2025-07-01 19:09:02 +0000

    multiple

--- src/addition.py
+++ src/addition.py
@@ -3,6 +3,13 @@
 def add(a,b):
     return a+ b;
 
+
+def mul(a,b):
+    return a*b
+
+def test_mul():
+    assert mul(2,3)==6
+    assert mul (4,5)==20
 def test_add():
     assert add(1,2)==3
     assert add(1,-1)==0
